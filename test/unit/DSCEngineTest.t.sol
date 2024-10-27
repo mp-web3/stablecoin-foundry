2dafd1d96f68743d6aea32e36ad326a2e8366182
mp-web3 <mp.web3.t@gmail.com>
2024-10-27 16:30:14 +0100

    testRevertsIfCollateralZero

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -7,6 +7,7 @@ import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "../mocks/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
@@ -17,18 +18,34 @@ contract DSCEngineTest is Test {
     address ethUsdPriceFeed;
     address weth;
 
+    address public USER = makeAddr("user");
+    uint256 public constant AMOUNT_COLLATERAL = 10e18;
+    uint256 public constant STARTING_ERC20_BALANCE = 10e18;
+
     function setUp() public {
         deployer = new DeployDSC();
         (dsc, dscEngine, helperConfig) = deployer.run();
         (ethUsdPriceFeed,, weth,,) = helperConfig.activeNetworkConfig();
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
     }
 
-    // PRICE TEST //
+    // PRICE TESTS //
     function testGetTokenValueInUSD() public {
         uint256 ethAmount = 10 ether;
         // 10e18 * 2000/ETH = 20,000e18
         uint256 expectedValueInUSD = 20_000e18;
         uint256 actualValueInUSD = dscEngine.getTokenValueInUSD(weth, ethAmount);
         assertEq(expectedValueInUSD, actualValueInUSD);
+    }
+
+    // DEPOSIT COLLATERAL TESTS //
+
+    function testRevertsIfCollateralZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dscEngine), AMOUNT_COLLATERAL);
+
+        vm.expectRevert(DSCEngine.DSCEngine_NeedsMoreThanZero.selector);
+        dscEngine.depositCollateral(weth, 0);
+        vm.stopPrank();
     }
 }
